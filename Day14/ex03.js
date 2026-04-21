f8ba1c55b6b495312ae85b0c9762b32cad5e2a60
Nguyen Hoang Tan <tannguyenhoang131@gmail.com>
2026-04-21 21:26:31 +0700

    Bài tính thuế nộp v3

--- Day14/ex03.js
+++ Day14/ex03.js
@@ -1,30 +1,44 @@
 /**
  * *Input salary input
  * ! Dùng if else cho từng trường hợp,
- * ! khi tính ta sẽ xuất ra số thuế phải
- * ! tính, xong dùng cái số đó tính thẳng luôn
- * ! console log ra số thuế
+ * ! nộp 5% có nghĩa là lấy số taxpercent là: salary * 5 / 100
+ * ! 14 triệu ở mốc 1 sẽ cho 5 % thuế, phần còn lại là ví dụ 30tr sẽ còn 16 triệu là nhân 10%
+ * ! biểu diễn là (25 - 11) * 5 / 100 + ( salary - 14 * 10)
  * ? xuất ra số thuế phải đóng
  */
 
 const salary = +prompt("salary?");
 function calcTax(salary) {
+  if (isNaN(salary) || salary < 0) {
+    return "Không hợp lệ";
+  }
+  let tax = 0;
+  let tax_point_1 = ((salary - 11000000) * 5) / 100;
+  let tax_point_2 =
+    ((25000000 - 11000000) * 5) / 100 + ((salary - 25000000) * 10) / 100;
+  let tax_point_3 =
+    ((25000000 - 11000000) * 5) / 100 +
+    ((50000000 - 25000000) * 10) / 100 +
+    ((salary - 50000000) * 20) / 100;
+  let tax_point_4 =
+    ((25000000 - 11000000) * 5) / 100 +
+    ((50000000 - 25000000) * 10) / 100 +
+    ((80000000 - 50000000) * 20) / 100 +
+    ((salary - 80000000) * 30) / 100;
   if (salary <= 11000000) {
     return 0;
   }
   if (salary <= 25000000) {
-    return salary / 5;
+    return tax_point_1;
   }
   if (salary <= 50000000) {
-    return salary / 10;
+    return tax_point_2;
   }
   if (salary <= 80000000) {
-    return salary / 20;
+    return tax_point_3;
   }
   if (salary > 80000000) {
-    return salary / 30;
-  } else {
-    return "Lương không hợp lệ";
+    return tax_point_4;
   }
 }
 
